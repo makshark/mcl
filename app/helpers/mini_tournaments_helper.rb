104d4f85c746fe4d34faf268a2bc022136813361
Vitalii Makovskyi <vitalii.makovskyi@gmail.com>
2016-02-15 04:32:24 +0200

    Add fvicon, main image, mini tournament helper

--- app/helpers/mini_tournaments_helper.rb
+++ app/helpers/mini_tournaments_helper.rb
@@ -1,2 +1,11 @@
 module MiniTournamentsHelper
+
+  # Проверяет, есть ли у числа остаток, если остаток равен 0 - то число выводится без остатка
+  # Так решил сделать Yes
+  def print_points_without_zero(point)
+    point.to_s.split('.').last == '0' ? point.to_s.split('.').first : point
+  end
+
+
+
 end
