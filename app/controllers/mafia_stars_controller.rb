52668be7e0ebfaf5234ac16f4aaa12712566612f
Vitalii Makovskyi <vitalii.makovskyi@gmail.com>
2016-02-21 11:23:41 +0200

    Fix bug with feature combine with hard card processing

--- app/controllers/mafia_stars_controller.rb
+++ app/controllers/mafia_stars_controller.rb
@@ -65,13 +65,13 @@ class MafiaStarsController < ApplicationController
           @additional_coefficient = (games_city_victories - games_mafia_victories).to_f / 2.0 * 0.1
           @hard_card = 'за мафию'
           # Получаем колличество игр выигранных игроком за мафию
-          player_win_mafia = GamePlayer.joins(:game).where('games.big_tournament_tour_id = (?) AND games.victory = 1 AND (game_players.role = 0 OR game_players.role = 2)', @tour.id).count
+          player_win_mafia = GamePlayer.joins(:game).where('games.big_tournament_tour_id = (?) AND games.victory = 1 AND (game_players.role = 0 OR game_players.role = 2) AND game_players.player_id = (?)', @tour.id, player[0]).count
           object[:rating] += @additional_coefficient * player_win_mafia
         else
           @additional_coefficient = (games_mafia_victories - games_city_victories).to_f / 2.0 * 0.1
           @hard_card = 'за мирных'
           # Получаем колличество игр выигранных игроком за мирных
-          player_win_citizen = GamePlayer.joins(:game).where('games.big_tournament_tour_id = (?) AND games.victory = 1 AND (game_players.role = 1 OR game_players.role = 3)', @tour.id).count
+          player_win_citizen = GamePlayer.joins(:game).where('games.big_tournament_tour_id = (?) AND games.victory = 1 AND (game_players.role = 1 OR game_players.role = 3) AND game_players.player_id = (?)', @tour.id, player[0]).count
           object[:rating] += @additional_coefficient * player_win_citizen
         end
 
