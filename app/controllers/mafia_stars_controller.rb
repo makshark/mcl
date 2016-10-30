28b4b14ae1ba7370f946ed678d6673a2edc727f9
Vitalii Mak <vitalii.makovskyi@gmail.com>
2016-10-30 03:23:25 +0200

    Temporary hardcode for mcl id 13

--- app/controllers/mafia_stars_controller.rb
+++ app/controllers/mafia_stars_controller.rb
@@ -99,6 +99,15 @@ class MafiaStarsController < ApplicationController
       object = {}
     end
     @result_array = @result_array.sort_by { |hsh| hsh[:rating] }.reverse!
+
+    ###### Сильный хардкод, для того что бы в одной из серий mcl поменять местами 1 и 2 место (разница по лучшим)
+    ## на логику приложения никак не влияет, в будущем - переделается!
+    if params[:id] == '12'
+      # меняем местами лачу и борща
+      @result_array.insert(0, @result_array.delete_at(@result_array[1]))
+    end
+
+    ######
     @games = Game.where(big_tournament_tour_id: params[:id]).order(:created_at)
     # TODO: разобраться, почему не работает, когда обращаюь через mafia, city (в поле victory)
     @total_victory_citizen = Game.where(big_tournament_tour_id: params[:id], victory: 0).count
