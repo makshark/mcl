99fc6056ac9c205eb9a8d61f088f9f5ab09a7c9a
Vitalii Mak <vitalii.makovskyi@gmail.com>
2017-02-01 11:55:27 +0200

    Hardcode studliga JUN

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -21,16 +21,16 @@ class GamesController < ApplicationController
     # players_points_count = GamePlayer.group(:player_id)
     #                            .sum(:points)
     # + players_points_count = GamePlayer.joins(:game).where('games.students_league = true').group(:player_id).sum(:points)
-    players_points_count = GamePlayer.joins(:game).where('games.students_league = true AND extract(month from games.date) = 12').group(:player_id).sum(:points)
+    players_points_count = GamePlayer.joins(:game).where('games.students_league = true AND extract(month from games.date) = 1').group(:player_id).sum(:points)
     # players_penalty_amount = GamePlayer.group(:player_id).sum(:penalty_amount)
     players_points_count.each do |player|
       # game_count = GamePlayer.where(player_id: player[0]).count
       # + game_count = GamePlayer.joins(:game).where('game_players.player_id = (?)', player[0]).where('games.students_league = true').count
-      game_count = GamePlayer.joins(:game).where('game_players.player_id = (?) ', player[0]).where('games.students_league = true AND extract(month from games.date) = 12').count
+      game_count = GamePlayer.joins(:game).where('game_players.player_id = (?) ', player[0]).where('games.students_league = true AND extract(month from games.date) = 1').count
       nick = Player.where(id: player[0]).first
       # object[:penalty_amount] = GamePlayer.where(player_id: player[0]).sum(:penalty_amount)
       # + object[:penalty_amount] = GamePlayer.joins(:game).where('game_players.player_id = (?)', player[0]).where('games.students_league = true').sum(:penalty_amount)
-      object[:penalty_amount] = GamePlayer.joins(:game).where('game_players.player_id = (?)', player[0]).where('games.students_league = true AND extract(month from games.date) = 12').sum(:penalty_amount)
+      object[:penalty_amount] = GamePlayer.joins(:game).where('game_players.player_id = (?)', player[0]).where('games.students_league = true AND extract(month from games.date) = 1').sum(:penalty_amount)
       # 0.25 - это дополнительный коефициент за колличество игр
       rating = ((player[1] / game_count.to_f) * 100 + object[:penalty_amount] * (-0.5)) + (0.25 * game_count.to_f)
       object[:game_count] = game_count
@@ -42,7 +42,7 @@ class GamesController < ApplicationController
     @result_array = @result_array.sort_by { |hsh| hsh[:rating] }.reverse!
     # Получаем колличество игр, которые необходимо сыгать для рейтинга
     # + game_count = (Game.where(students_league: true).count.to_f / 100) * 20
-    game_count = (Game.where(students_league: true).where('extract(month from date) = ?', 12).count.to_f / 100) * 20
+    game_count = (Game.where(students_league: true).where('extract(month from date) = ?', 1).count.to_f / 100) * 20
     position = 1
     # В будущем вынести это в настройки к каждому игроку!!!!!
     banned_nicks = ['Клайд']
